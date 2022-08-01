d2fa90895f921b44004c715ff986e8378af62d76
Rachel Kelly <racheldk126@gmail.com>
2022-08-01 14:41:51 -0700

    solid and empty heart icons added to Card component to indicate following or not. onClick calls functions that will send axios requests when we have that from BE

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,28 +1,54 @@
 import { Link } from "react-router-dom";
+import { FaHeart } from "react-icons/fa";
+import { FaRegHeart } from "react-icons/fa";
 
-function Card({cards}) {
+function Card({ cards }) {
+    const following = ["connor", "admin"];
+
+    const handleFollow = () => {
+        console.log("follow");
+        // axios request will go here
+    };
+
+    const handleUnfollow = () => {
+        console.log();
+        // axios request will go here
+    };
 
     return (
         <div>
             <div className="card-list">
-            {cards.map((card) =>
-
-                <div className="card messageBox" >
-
-                    <div className={`card-text bg-${card.bg_color} border-${card.border_color}`}>
-                        <div className={`card-title ${card.font_color} ${card.font}`}>
-                            {card.title}
+                {cards.map((card) => (
+                    <div className="card messageBox">
+                        <div
+                            className={`card-text bg-${card.bg_color} border-${card.border_color}`}
+                        >
+                            <div
+                                className={`card-title ${card.font_color} ${card.font}`}
+                            >
+                                {card.title}
+                            </div>
+                            <div className={`${card.font_color} ${card.font}`}>
+                                {card.message}
+                            </div>
                         </div>
-                        <div className={`${card.font_color} ${card.font}`}>
-                            {card.message}
-                        </div>
+
+                        {/* if card.username appears in following array, show filled heart icon. if not, show outline heart icon. clicking on the icon sends POST request to API and changes  */}
+
+                        <Link
+                            to={`/user/${card.user_id}`}
+                            className="btn btn-primary"
+                        >
+                            {card.username}
+                        </Link>
+
+                        {following.includes(`${card.username}`) ? (
+                            <FaHeart onClick={handleUnfollow()} />
+                        ) : (
+                            <FaRegHeart onClick={handleFollow()} />
+                        )}
                     </div>
-
-                    <Link to={`/user/${card.user_id}`} className="btn btn-primary">Author: {card.username}</Link>
-
-                    {/* add profile link to user */}
-                </div>
-            )}
+                ))}
             </div>
         </div>
     );
