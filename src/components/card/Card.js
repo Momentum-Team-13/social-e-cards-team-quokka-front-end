a7ed68f941f0d8b87be99dfc4e9912a20bffb762
Rachel Kelly <racheldk126@gmail.com>
2022-08-03 09:33:25 -0700

    card title and message has text centered

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -10,11 +10,11 @@ function Card({ cards }) {
                             className={`card-text bg-${card.bg_color} border-${card.border_color}`}
                         >
                             <div
-                                className={`card-title ${card.font_color} ${card.font}`}
+                                className={`card-title has-text-centered ${card.font_color} ${card.font}`}
                             >
                                 {card.title}
                             </div>
-                            <div className={`${card.font_color} ${card.font}`}>
+                            <div className={`has-text-centered ${card.font_color} ${card.font}`}>
                                 {card.message}
                             </div>
                         </div>
