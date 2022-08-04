17120565f30092367c12f0626185f62a0569aed4
An <an.tle.914@gmail.com>
2022-08-03 20:39:57 -0400

    follow button partially works, unfollow does not

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,55 +3,67 @@ import "./navbar.css";
 import "bulma/css/bulma.min.css";
 
 export default function Navbar({ handleLogout }) {
-  return (
-    <nav>
-      <label className="navlogo">QuokkaCards</label>
-      <ul>
-        <li>
-          <div className="navlink">
-            <Link to={"/"}>Home</Link>
-          </div>
-        </li>
-        <li>
-          <div className="navlink">
-            <Link to={"/explore"}>Explore</Link>
-          </div>
-        </li>
-        <li>
-          <div className="navlink">
-            <Link to={"/MyCards"}>My Cards</Link>
-          </div>
-        </li>
-        <li>
-          <div className="navlink">
-            <Link to={"/NewCard"}>New Card</Link>
-          </div>
-        </li>
-        <li>
-          <div className="navlink" onClick={handleLogout}>
-            <Link to={"/"}>Sign Out</Link>
-          </div>
-        </li>
-      </ul>
-    </nav>
-    // <div className="header">
-    //     <button className="sign-out button is-small" onClick={handleLogout}>
-    //     <Link to={"/"}>Sign Out</Link></button>
-    //     <div className="app-title">QuokkaCards</div>
-    //     <div className="nav">
-    //         <div className="">
-    //             <Link to={"/"}>Home</Link>
-    //         </div>
-    //         <div className="">
-    //             <Link to={"/explore"}>Explore</Link>
-    //         </div>
-    //         <div className="">
-    //             <Link to={"/MyCards"}>My Cards</Link>
-    //         </div>
-    //         <div className="">
-    //             <Link to={"/NewCard"}>New Card</Link>
-    //         </div>
-    //     </div>
-    // </div>
-  );
+    return (
+            <nav className="nav">
+                <label className="navlogo">QuokkaCards</label>
+                <ul>
+                    <li>
+                        <div>
+                            <Link to={"/"} className="navlink">
+                                Home
+                            </Link>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <Link to={"/explore"} className="navlink">
+                                Explore
+                            </Link>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <Link to={"/MyCards"} className="navlink">
+                                My Cards
+                            </Link>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <Link to={"/NewCard"} className="navlink">
+                                New Card
+                            </Link>
+                        </div>
+                    </li>
+                    <li>
+                        <div onClick={handleLogout}>
+                            <Link to={"/"} className="navlink">
+                                Sign Out
+                            </Link>
+                        </div>
+                    </li>
+                </ul>
+                {/* <div className="nav-bottom-padding"></div> */}
+            </nav>
+        
+        // <div className="header">
+        //     <button className="sign-out button is-small" onClick={handleLogout}>
+        //     <Link to={"/"}>Sign Out</Link></button>
+        //     <div className="app-title">QuokkaCards</div>
+        //     <div className="nav">
+        //         <div className="">
+        //             <Link to={"/"}>Home</Link>
+        //         </div>
+        //         <div className="">
+        //             <Link to={"/explore"}>Explore</Link>
+        //         </div>
+        //         <div className="">
+        //             <Link to={"/MyCards"}>My Cards</Link>
+        //         </div>
+        //         <div className="">
+        //             <Link to={"/NewCard"}>New Card</Link>
+        //         </div>
+        //     </div>
+        // </div>
+    );
 }
--- src/components/pages/userCardsPage/UserCardsPage.js
+++ src/components/pages/userCardsPage/UserCardsPage.js
@@ -1,17 +1,115 @@
 import Sidebar from "../../sidebar/Sidebar";
-import Card from "../../card/Card";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams, Link } from "react-router-dom";
+// import Card from "../../card/Card";
 // import { useEffect } from "react";
 // import { useParams } from 'react-router-dom'
 
-export default function UserCards() {
-    return (
-        <div className="container">
-            <div></div>
-            <Sidebar  />
-            <div>
-                All Cards
-                <Card  />
+export default function UserCards({ token, allUsers }) {
+  const { id } = useParams();
+  // console.log({id})
+  const [userCardList, setUserCardList] = useState([]);
+  const [toggleFollow, setToggleFollow] = useState(false);
+  const [error, setError] = useState(null);
+
+  const toggleFollowButton = () => {
+    setToggleFollow(!toggleFollow);
+  };
+
+  const handleFollow = (event) => {
+    // console.log(regUsername, regPassword);
+    event.preventDefault();
+    setError(null);
+    axios
+      .post("https://quokka-cards.herokuapp.com/users/follow/", 
+        {
+            following: `${id}`
+        },
+        {
+        headers: {
+            Authorization: `Token ${token}`
+    },
+})
+      .then(() => {
+        // the following console.log is so that the variable error is used on the page and we stop getting warnings about unused variables so our app will deploy on Netlify
+        console.log(error);
+        toggleFollowButton();
+      })
+      .catch((error) => {
+        setError(error.message);
+        alert(error);
+      });
+  };
+
+  const handleUnfollow = (event) => {
+    // console.log(regUsername, regPassword);
+    event.preventDefault();
+    setError(null);
+    axios
+      .delete(`https://quokka-cards.herokuapp.com/users/unfollow/${id}`,
+        {
+        headers: {
+            Authorization: `Token ${token}`
+    },
+})
+      .then(() => {
+        // the following console.log is so that the variable error is used on the page and we stop getting warnings about unused variables so our app will deploy on Netlify
+        console.log(error);
+        toggleFollowButton();
+      })
+      .catch((error) => {
+        setError(error.message);
+        alert(error);
+      });
+  };
+
+  // axios request here to get user's cards
+  useEffect(() => {
+    axios
+      .get(`https://quokka-cards.herokuapp.com/users/${id}`, {
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+      })
+      .then((res) => {
+        // console.log(res.data);
+        setUserCardList(res.data);
+      });
+  }, []);
+
+  const sidebarTitle = "All QuokkaCards Users";
+  return (
+    <>
+      <Sidebar userNames={allUsers} title={sidebarTitle} />
+      <div className="card-list-container">
+        <div className="card-list">
+          {userCardList.map((card) => (
+            <div className="card messageBox">
+              <div
+                className={`card-text bg-${card.bg_color} border-${card.border_color}`}
+              >
+                <div
+                  className={`card-title has-text-centered  ${card.font_color} ${card.font}`}
+                >
+                  {card.title}
+                </div>
+                <div
+                  className={`card-message has-text-centered ${card.font_color} ${card.font}`}
+                >
+                  {card.message}
+                </div>
+              </div>
+              <div>Author: {card.username}</div>
             </div>
+          ))}
         </div>
-    );
+      </div>
+      {toggleFollow === false ? (
+        <button onClick={handleFollow} className="follow-button">Follow</button>
+      ) : (
+        <button onClick={handleUnfollow} className="follow-button">Don't Follow</button>
+      )}
+    </>
+  );
 }
