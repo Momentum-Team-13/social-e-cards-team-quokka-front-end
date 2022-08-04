358b7534f031898d00d80f4504859b502517bebd
Rachel Kelly <racheldk126@gmail.com>
2022-08-04 07:26:02 -0700

    search for users on home page, explore, mycards, and users cards

--- src/components/pages/homePage/HomePage.js
+++ src/components/pages/homePage/HomePage.js
@@ -2,24 +2,50 @@ import "./homePage.css";
 import Card from "../../card/Card";
 import "../../sidebar/sidebar.css";
 import {Link} from 'react-router-dom'
-
+import { useState } from "react";
 
 export default function Home({ followCardList, following, handleUserCardList }) {
   const sidebarTitle = "People I follow";
+  const [query, setQuery] = useState("");
+
   return (
     <>
       <div className="sidebar-container">
                 <div className="sidebar">
                     <div className="sidebar-title">{sidebarTitle}</div>
                     <div className="sidebar-list-container">
-                        {following.map((user) => (
-                            <div>
-                                {console.log(user)}
-                                <Link to={`/users/${user.following_id}`} className="list">
-                                    {user.following_username}
-                                </Link>
-                            </div>
-                        ))}
+
+                    <div className="search">
+                        <label className="visually-hidden">Search for a User</label>
+                        <input
+                            placeholder="Search for a user"
+                            onChange={(event) => setQuery(event.target.value)}
+                            className="term input"
+                        />
+                        {following
+                            .filter((user) => {
+                                if (query === "") {
+                                    return user.username;
+                                } else if (
+                                    user.username
+                                        .toLowerCase()
+                                        .includes(query.toLocaleLowerCase())
+                                ) {
+                                    return user.username;
+                                }
+                            })
+                            .map((user) => (
+                                <div>
+                                    {console.log(user)}
+                                    <Link
+                                        to={`/users/${user.id}`}
+                                        className="list"
+                                    >
+                                        {user.username}
+                                    </Link>
+                                </div>
+                            ))}
+                    </div>
                     </div>
                 </div>
             </div>
